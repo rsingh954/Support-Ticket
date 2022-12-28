6a4ee2f6692ae151e02ca6aad43a97d7661dabdd
rsingh954 <singhrajaca@gmail.com>
2022-12-28 13:59:07 -0800

    added new ticket form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,31 @@
 /* eslint-disable no-unused-vars */
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import NewTicket from "./pages/NewTicket";
 import Register from "./pages/Register";
 
 function App() {
   return (
     <>
-        <Router>
-          <div className="container">
-            <Header />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-            </Routes>
-          </div>
-        </Router>
-        <ToastContainer/>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/new-ticket" element={<PrivateRoute />}>
+              <Route path="/new-ticket" element={<NewTicket />} />
+            </Route>
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
     </>
   );
 }
